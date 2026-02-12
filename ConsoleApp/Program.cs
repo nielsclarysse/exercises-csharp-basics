31738a79b4929cc2754501b131dc697362e04120
Niels Clarysse <niels.clarysse@student.howest.be>
2026-02-12 14:06:33 +0100

    EX0 - Warm Up

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// *****************
+// * EX0 - Warm Up *
+// *****************
+
+IEnumerable<int> Fibonacci(int length)
+{
+    (int a, int b) = (1, 1);
+
+    for (int i = 0; i < length; i++)
+    {
+        yield return a;
+
+        (a, b) = (b, a + b);
+    }
+}
+
+foreach (int i in Fibonacci(10))
+{
+    Console.WriteLine(i);
+}
